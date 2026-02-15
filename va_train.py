22654ec3af7075d6ad73dcfc168bd00cc369728d
longtaizi13579 <3514748698@qq.com>
2026-02-15 08:59:12 +0800

    bug fix

--- va_train.py
+++ va_train.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, DistributedSampler
-
+import torch.optim as optim
 import deepspeed
 from tqdm import tqdm
 from peft import LoraConfig, TaskType, get_peft_model
